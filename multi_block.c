a1aa4043c9db0b09f9652fe2a4b8921c0b1def8f
whit3vill <whit3vill@gmail.com>
2017-11-07 21:43:20 -0800

    b

--- multi_block.c
+++ multi_block.c
@@ -32,13 +32,15 @@ struct bintree *search (struct bintree *root, int val) {
 
 int myhash(const char *string) {
 	int i;
-	int num = 0;
+	unsigned int num = 0;
 
 	unsigned char result[MD5_DIGEST_LENGTH];
 
+
+
 	MD5(string, strlen(string), result);
 
-	for (i=0; i<4; i++) {
+	for (i=0; i<16; i++) {
 		printf("%02x", result[i]);
 		num += (int)(result[i]) * (int)(pow((double)(16), (double)(6-2*(i))));
 	}
@@ -63,26 +65,29 @@ void main (int argc, char *argv[]) {
 	FILE *fp;
 	char buf[BUFSIZE];
 	char *string;
+	char *line;
+	int count;
 
 	filename = argv[1];
 	fp = fopen (filename, "r");
 	if (fp == NULL)	printf("No %s in directory.\n", filename);
 
-	while (fgets(buf, BUFSIZE, fp)) {
-        char* tmp = strdup(buf);
-        //strncpy(string, getfield(tmp, 2), strlen(getfield(tmp, 2)));
-        // NOTE strtok clobbers tmp
-        
-        ////
-        printf("%s\n", getfield(tmp, 2));
-        string = "asdf";
-
-        ////
-        printf("%s\n", string);
-        printf("%d\n", myhash(string));
-        free(tmp);
+	while ((line = fgets(buf, BUFSIZE, fp)) != NULL) {
+		string = strtok(line, ",");
+		count = 0;
+		while (string) {
+			count++;
+			if (count == 2) {
+				string[strlen(string)-1] = 0;
+				printf("%s\n", string);
+				printf("%u\n", myhash(string));
+			}
+			string = strtok(NULL, ",");
+		}
     }
-
+    string = "google.com";
+    printf("%u\n", myhash(string));
+    printf("%u\n", myhash("google.com"));
 	insert(&root, 9);
 	insert(&root, 4);
 	insert(&root, 15);
@@ -90,6 +95,5 @@ void main (int argc, char *argv[]) {
     insert(&root, 12);
     insert(&root, 17);
     insert(&root, 2);
-
 }
 
